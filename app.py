11553ebd85e2a2a0b7e514b834e0f2d3156a6ccb
zahid2426 <zahidzubair03@gmail.com>
2025-04-27 00:26:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,3 +52,11 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
